efc9dfb28c50b75ac69f66de336c7968df3bbf21
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2021-12-17 09:41:12 +0300

    Update random_sayı_atan_kod.cpp

--- random_sayı_atan_kod.cpp
+++ random_sayı_atan_kod.cpp
@@ -9,7 +9,7 @@ int main() {
 	srand(time(NULL));
 	//ard arda aynı sayı gelmemesi için randomu sıfırlayan komut
 	int rastgelesayi = rand() % 100;
-	//random atmaya yarayan komut
+	//random atmaya yarayan komut. % 100 ifadesi 0 ile 100 arasında bir sayi atanmasını sağlar.
 	cout << rastgelesayi;
 	
 }
