db331f7737a331776497d60d72a39aee744b76bd
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2021-12-22 12:54:28 +0300

    Create sekil ödevi sondaki şekil.cpp

---
+++ sekil ödevi sondaki şekil.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+void yıldız6() {
+
+
+	for (int j = 0; j < 1; j++)
+	{
+		for (int i = 0; i < 3; i++) {
+			cout << " ";
+		}
+
+
+		for (int i = 0; i < 6; i++) {
+			cout << "*";
+
+		}
+
+	}cout << endl;
+}
+void yıldız10()
+{
+	for (int j = 0; j < 1; j++)
+	{
+		for (int i = 0; i < 1; i++) {
+			cout << " ";
+		}
+
+
+		for (int i = 0; i < 10; i++) {
+			cout << "*";
+
+		}
+
+	}cout << endl;
+}
+
+void yıldız12(){
+{
+
+
+		for (int i = 0; i < 12; i++) {
+			cout << "*";
+
+		}
+
+	}cout << endl;
+}
+ int main(){
+	
+	 yıldız6 ();
+	 yıldız10();
+	 yıldız12();
+	 yıldız10();
+	 yıldız6();
+	 yıldız10();
+	 yıldız12();
+	 yıldız10();
+	 yıldız6();
+	 
+
+
+
+
+
+}
